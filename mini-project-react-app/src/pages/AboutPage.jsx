9f2bd79e6ed6525cb3ee2acb5b4cf060dce4909c
Johana Castellano <johanacg16@gmail.com>
2025-02-09 01:10:21 +0000

    Day 3 completed

--- mini-project-react-app/src/pages/AboutPage.jsx
+++ mini-project-react-app/src/pages/AboutPage.jsx
@@ -1,5 +1,25 @@
+import linkedInLogo from '../assets/images/linkedin.svg'
+import gitHubLogo from '../assets/images/github-light.svg'
+
 export default function AboutPage() {
     return (
-      <div>AboutPage</div>
+      <div>
+        <h1>To-Do App</h1>
+        <p>The To-do app is a React App that allows users to create and manage tasks, as well as check off the existing tasks when completed.</p>
+        <div className="team-members">
+          <div>
+            <img src="../src/assets/images/profile-icon.png" alt="default profile icon" />
+            <h3>Celina</h3>
+            <a className="social-link" href=''><img className='linkedin-logo' src={linkedInLogo} alt="linkedin logo" />LinkedIn</a>
+            <a className="social-link" href=''><img className='github-logo' src={gitHubLogo} alt="github logo" />GitHub</a>
+          </div>
+          <div>
+            <img src="../src/assets/images/profile-icon.png" alt="default profile icon" />
+            <h3>Johana</h3>
+            <a className="social-link" href=''><img className='linkedin-logo' src={linkedInLogo} alt="linkedin logo" />LinkedIn</a>
+            <a className="social-link" href=''><img className='github-logo' src={gitHubLogo} alt="github logo" />GitHub</a>
+          </div>
+        </div>
+      </div>
     )
 }
