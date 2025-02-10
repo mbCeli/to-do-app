c7e5929ceb93b88ec1f0a31f2100a3678d34a122
Johana Castellano <johanacg16@gmail.com>
2025-02-10 16:59:52 +0000

    starting update form in to do details page

--- mini-project-react-app/src/pages/ToDoDetails.jsx
+++ mini-project-react-app/src/pages/ToDoDetails.jsx
@@ -1,4 +1,5 @@
 import { useParams, Link } from "react-router-dom";
+import { useState } from "react";
 
 import ToDoTasks from "../assets/to-dos-descrip.json";
 
@@ -7,6 +8,11 @@ export default function ToDoDetails() {
 
   const index = Number(taskIndex); //Los parámetros de la URL como {taskIndex} se pasan como strings, así que necesitamos convertirlo a un número antes de hacer comparaciones
   const taskDetails = ToDoTasks.find((task, i) => i === index);
+
+  //States para el formulario de actualizar:
+  const [task, setTask] = useState(taskDetails.task);
+  const [description, setDescription] = useState(taskDetails.description);
+  const [priority, setPriority] = useState(taskDetails.priority);
 
   return (
     <div className="to-do-details">
@@ -18,6 +24,43 @@ export default function ToDoDetails() {
             <p className="description">{taskDetails.description}</p>
             <p>{taskDetails.priority}</p>
             {/* Poner un input para que se pueda escribir mas detalles sobre el task */}
+
+            <button>Update</button><br />
+
+            {/* aquí empezaría el elemento de formulario de update, que tendrá un atributo hidden hasta que hagamos click en el botón update */}
+
+            {/* <div>
+              <form onSubmit={handleFormSubmit} hidden>
+                <span>Add a New Task</span>
+                <div>
+                  <label>
+                    Task
+                    <input name="task" type="text" value={task} onChange={(e) => setTask(e.target.value)} placeholder="Task Title" />
+                  </label>
+
+                  <label>
+                    Description (optional)
+                    <textarea name="description" rows="5" cols="40" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Comment text."></textarea>
+                  </label>
+
+                </div>
+
+                <div>
+                  <label>
+                    Priority
+                    <select name="priority" value={priority} onChange={(e) => setPriority(e.target.value)}>
+                      <option value="">-- None --</option>
+                      <option value="Low">Low</option>
+                      <option value="Medium">Medium</option>
+                      <option value="High">High</option>
+                    </select>
+                  </label>
+
+                  <button type="submit">Add Task</button>
+                </div>
+
+              </form>
+            </div> */}
 
             <Link to="/">
               <button>Back</button>
