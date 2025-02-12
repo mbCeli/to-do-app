b91da5fc08cf9de6dbeda6751f0735958e69f96d
mbCeli <munbaporicelina@outlook.com>
2025-02-12 17:47:20 +0000

    show and hide update button completed

--- mini-project-react-app/src/pages/ToDoDetails.jsx
+++ mini-project-react-app/src/pages/ToDoDetails.jsx
@@ -9,15 +9,24 @@ export default function ToDoDetails() {
   const index = Number(taskIndex); //Los parámetros de la URL como {taskIndex} se pasan como strings, así que necesitamos convertirlo a un número antes de hacer comparaciones
   const taskDetails = ToDoTasks.find((task, i) => i === index);
 
-
   //Para cambiar entre mostrar/esconder el formulario de update
 
   const [showForm, setShowForm] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
   //States para el formulario de actualizar:
   const [task, setTask] = useState(taskDetails.task);
   const [description, setDescription] = useState(taskDetails.description);
   const [priority, setPriority] = useState(taskDetails.priority);
+
+  const handleUpdateForm = (e) => {
+    e.preventDefault();
+
+    const updatedTask = { 
+      task,  
+      description, 
+      priority };
+}
 
   return (
     <div className="to-do-details">
@@ -29,44 +38,67 @@ export default function ToDoDetails() {
             <p className="description">{taskDetails.description}</p>
             <p>{taskDetails.priority}</p>
             {/* Poner un input para que se pueda escribir mas detalles sobre el task */}
-
-            <button onClick={() => setShowForm(!showForm)}>Update</button><br />
-
+            {/* click on update button to show form and hide button when showing the form */}
+            <button
+              className={showButton ? "show-button" : "hide-button"}
+              onClick={() => {
+                setShowForm(!showForm);
+                setShowButton(!showButton);
+              }}
+            >
+              Update
+            </button>
+            <br />
             {/* aquí empezaría el elemento de formulario de update, que tendrá una variable state que hace que se muestre o no cuando hacemos click en el botón update */}
+            {showForm && (
+              <div>
+                <form /* onSubmit={handleUpdateForm} */>
+                  <span>Update this Task</span>
+                  <div>
+                    <label>
+                      Task
+                      <input
+                        name="task"
+                        type="text"
+                        value={task}
+                        onChange={(e) => setTask(e.target.value)}
+                        placeholder="Task Title"
+                      />
+                    </label>
 
-            {showForm && <div>
-              <form /* onSubmit={handleFormSubmit} */ >
-                <span>Add a New Task</span>
-                <div>
-                  <label>
-                    Task
-                    <input name="task" type="text" value={task} onChange={(e) => setTask(e.target.value)} placeholder="Task Title" />
-                  </label>
+                    <label>
+                      Description (optional)
+                      <textarea
+                        name="description"
+                        rows="5"
+                        cols="40"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder="Comment text."
+                      ></textarea>
+                    </label>
+                  </div>
 
-                  <label>
-                    Description (optional)
-                    <textarea name="description" rows="5" cols="40" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Comment text."></textarea>
-                  </label>
+                  <div>
+                    <label>
+                      Priority
+                      <select
+                        name="priority"
+                        value={priority}
+                        onChange={(e) => setPriority(e.target.value)}
+                      >
+                        <option value="">-- None --</option>
+                        <option value="Low">Low</option>
+                        <option value="Medium">Medium</option>
+                        <option value="High">High</option>
+                      </select>
+                    </label>
 
-                </div>
-
-                <div>
-                  <label>
-                    Priority
-                    <select name="priority" value={priority} onChange={(e) => setPriority(e.target.value)}>
-                      <option value="">-- None --</option>
-                      <option value="Low">Low</option>
-                      <option value="Medium">Medium</option>
-                      <option value="High">High</option>
-                    </select>
-                  </label>
-
-                  <button type="submit">Add Task</button>
-                </div>
-
-              </form>
-            </div>}
-
+                    <button type="submit">Save</button>
+                  </div>
+                </form>
+              </div>
+            )}
             <Link to="/">
               <button>Back</button>
             </Link>
