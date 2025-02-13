8bb5de1627c71b54a28b06ee0dc4726f3f575279
Johana Castellano <johanacg16@gmail.com>
2025-02-13 12:22:30 +0000

    basic css changes in navbar, sidebar and footer

--- mini-project-react-app/src/components/Footer.jsx
+++ mini-project-react-app/src/components/Footer.jsx
@@ -1,6 +1,9 @@
+import gitHubLogo from '../assets/images/github-mark-white.svg'
+
 export default function Footer() {
   return (
     <div className="footer">
+      <img className='github-logo' src={gitHubLogo} alt="github logo" />
       <a href="https://github.com/mbCeli/to-do-app">GitHub repository link</a>
     </div>
   );
